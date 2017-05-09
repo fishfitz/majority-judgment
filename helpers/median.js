610facf2e644069bddff52c791fd819b2ecd7b4c
Cobalt <toutcobalt@gmail.com>
2017-05-09 11:16:11 +0200

    Shorter version of median

--- helpers/median.js
+++ helpers/median.js
@@ -4,13 +4,10 @@
 */
 
 module.exports = function(values) {
-    if (values.length === 1) {
-        return values[0];
-    }
+    if (values.length === 1) return values[0];
 
     const half = Math.floor(values.length / 2);
-    if (values.length % 2) {
-        return values[half];
-    }
+    if (values.length % 2) return values[half];
+
     return Math.floor((values[half - 1] + values[half]) / 2.0);
 };
